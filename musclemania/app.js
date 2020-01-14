933d1b05195673a8f3bc2ee42f898feae57705b8
yogeshyadav1998 <yogeshbhakli@gmail.com>
2020-01-14 12:03:02 +0530

    add authentication

--- musclemania/app.js
+++ musclemania/app.js
@@ -1,15 +1,49 @@
 var express = require("express");
 var app = express();
+var passport = require("passport");
+var localstrategy = require("passport-local")
 var request = require("request");
 var bodyparser = require("body-parser");
 var mongoose = require("mongoose");
+var session = require("express-session");
+user = require("./models/user");
 
+// mongoose config
 mongoose.connect('mongodb://localhost:27017/musclemania', {useNewUrlParser: true});
 app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 
+var dietplan_reqSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    weight: Number,
+    height: Number,
+    profession: String,
+    imgurl: String,
+    dietplan: String
+});
+var dietplan_req = mongoose.model("dietplan_req", dietplan_reqSchema);
 
+// passport configuration
+app.use(session({
+    secret: "my name is yogesh",
+    resave: false,
+    saveUninitialized: false
+  }))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localstrategy(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+app.use(function(req, res, next){
+    res.locals.currentuser= req.user;
+    next();
+})
+
+
+
+//routes
 app.get("/",function(req,res){
     res.render("landing");
 });
@@ -38,26 +72,13 @@ app.get("/legs",function(req,res){
     res.send("here you get legs workout ecercises.")
 });
 
-var dietplan_reqSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    weight: Number,
-    height: Number,
-    profession: String,
-    imgurl: String,
-    dietplan: String
-});
-
-var dietplan_req = mongoose.model("dietplan_req", dietplan_reqSchema);
-
-
 app.get("/dietplan_reqs",function(req,res){
     dietplan_req.find({},function(err,dietplan_reqs){
         if(err){
             console.log(err)
         }
         else{
-            res.render("dietplan_reqs",{dietplans: dietplan_reqs});
+            res.render("dietplan_reqs",{dietplans: dietplan_reqs, user: req.user});
         }
     })
 });
@@ -97,6 +118,38 @@ app.get("/dietplan_reqs/:id",function(req,res){
     })
 });
 
+app.get("/register",function(req, res){
+    res.render("register")
+});
+
+app.post("/register",function(req, res){
+    user.register(new user({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/");
+        })
+    })
+})
+
+app.get("/login",function(req, res){
+    res.render("login")
+});
+
+app.post("/login",passport.authenticate("local",
+{
+    successRedirect: "/",
+    failureRedirect: "/login"
+}),function(req, res){
+
+});
+
+app.get("/logout",function(req, res){
+    req.logout();
+    res.redirect("/");
+});
 
 
 
@@ -112,9 +165,12 @@ app.get("/dietplan_reqs/:id",function(req,res){
 
 
 
-
-
-
+function islogedin(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 app.listen(3000,function(){
